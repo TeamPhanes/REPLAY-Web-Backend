043d3d97380dcba53ac9f0a57500b0f746a82300
rave189 <rave189@naver.com>
2025-04-27 01:05:04 +0900

    remove user code

--- src/main/java/phanes/replay/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package phanes.replay.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import phanes.replay.user.domain.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
