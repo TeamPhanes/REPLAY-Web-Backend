6d5097ecdea92f8c8dc22da7cf613fde56e5a392
rave189 <59406960+rave189@users.noreply.github.com>
2025-04-27 17:41:34 +0900

    Feature/swagger (#3)
    * add swagger library
    
    * add swagger
    - add swagger uri permit
    - add swagger config

---
+++ src/main/java/phanes/replay/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package phanes.replay.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SERVICE_NAME = "Replay Project";
+    private static final String API_VERSION = "v1";
+    private static final String API_DESCRIPTION = "Replay Project API Swagger";
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title(SERVICE_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION);
+    }
+}
