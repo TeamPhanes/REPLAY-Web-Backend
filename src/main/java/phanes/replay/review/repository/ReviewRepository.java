2a8da47356c8258e99a95dd69594e5243fdf5fdc
rave189 <rave189@naver.com>
2025-04-27 14:54:09 +0900

    remove no used file

--- src/main/java/phanes/replay/review/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package phanes.replay.review.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import phanes.replay.review.domain.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
