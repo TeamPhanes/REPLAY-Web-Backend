5e7c8c81bb54674c356c1d3b479bdedc959b79fa
rave189 <rave189@naver.com>
2025-04-28 23:34:22 +0900

    add image upload logic

--- src/main/java/phanes/replay/user/service/UserService.java
+++ src/main/java/phanes/replay/user/service/UserService.java
@@ -4,15 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import phanes.replay.exception.ImageUploadFailException;
 import phanes.replay.exception.UserNotFoundException;
 import phanes.replay.gathering.domain.Role;
 import phanes.replay.gathering.repository.Gathering_MemberRepository;
+import phanes.replay.image.service.S3Service;
 import phanes.replay.review.repository.ReviewRepository;
 import phanes.replay.roomescape.repository.RoomEscapeParticipateRepository;
 import phanes.replay.user.domain.User;
 import phanes.replay.user.dto.UserDTO;
 import phanes.replay.user.mapper.UserMapper;
 import phanes.replay.user.repository.UserRepository;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +27,7 @@ public class UserService {
     private final Gathering_MemberRepository gatheringMemberRepository;
     private final RoomEscapeParticipateRepository roomEscapeParticipateRepository;
     private final ReviewRepository reviewRepository;
+    private final S3Service s3Service;
     private final UserMapper userMapper;
 
     public UserDTO getUser(Long userId) {
@@ -36,9 +42,14 @@ public class UserService {
 
     @Transactional
     public void updateUser(Long userId, MultipartFile image, String nickname, String comment) {
-        
+        String imageUrl;
+        try {
+            imageUrl = s3Service.uploadImage("replay", "images/" + UUID.randomUUID()+".png", image);
+        } catch (IOException e) {
+            throw new ImageUploadFailException("image upload fail");
+        }
         User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("user not found"));
-        user.updateUserInfo("", nickname, comment);
+        user.updateUserInfo(imageUrl, nickname, comment);
         userRepository.save(user);
     }
 }
