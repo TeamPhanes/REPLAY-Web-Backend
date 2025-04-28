22840878015b971cbdd55ba20c22752c71575389
rave189 <rave189@naver.com>
2025-04-28 19:31:47 +0900

    fix typo - domain을 참조하도록 수정 - user를 참조하도록 수정

--- src/main/java/phanes/replay/roomescape/repository/RoomEscapeParticipateRepository.java
+++ src/main/java/phanes/replay/roomescape/repository/RoomEscapeParticipateRepository.java
@@ -2,8 +2,9 @@ package phanes.replay.roomescape.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import phanes.replay.roomescape.domain.RoomEscapeParticipate;
 
 @Repository
-public interface RoomEscapeParticipateRepository extends JpaRepository<RoomEscapeParticipateRepository, Long> {
+public interface RoomEscapeParticipateRepository extends JpaRepository<RoomEscapeParticipate, Long> {
     Long countByUserId(Long userId);
 }
