274305a0ff8b651668e836b6abc150deea6bae29
rave189 <rave189@naver.com>
2025-04-29 18:55:32 +0900

    refactor my profile logic - repository 직접 참조가 아닌 service 참조로 수정

---
+++ src/main/java/phanes/replay/review/ReviewService.java
@@ -0,0 +1,16 @@
+package phanes.replay.review;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import phanes.replay.review.repository.ReviewRepository;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    public Long getCountBySuccess(Boolean success) {
+        return reviewRepository.countBySuccess(success);
+    }
+}
