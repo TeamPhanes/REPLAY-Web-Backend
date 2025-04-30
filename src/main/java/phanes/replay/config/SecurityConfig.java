be8615e80802dd5f47c41dea269093a13fe36f15
PeterShin <daehun47@gmail.com>
2025-04-30 17:56:00 +0900

    refactor

--- src/main/java/phanes/replay/config/SecurityConfig.java
+++ src/main/java/phanes/replay/config/SecurityConfig.java
@@ -28,9 +28,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/v3/api-docs.yaml",
-                                "/v3/api-docs.yaml",
                                 "/gathering/**",
-                                "/room"
+                                "/room/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
