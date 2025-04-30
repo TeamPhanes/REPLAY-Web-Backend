1ad950dc98e64172b76084aa4c830d7eb400f6f8
PeterShin <daehun47@gmail.com>
2025-04-30 17:55:35 +0900

    ThemeServiceImpl getThemes method

---
+++ src/main/java/phanes/replay/theme/service/ThemeServiceImpl.java
@@ -0,0 +1,30 @@
+package phanes.replay.theme.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import phanes.replay.theme.dto.ThemeListResponse;
+import phanes.replay.theme.dto.ThemeSearchRequest;
+import phanes.replay.theme.repository.ThemeRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ThemeServiceImpl implements ThemeService {
+
+    private final ThemeRepository themeRepository;
+
+    @Override
+    public List<ThemeListResponse> getThemes(ThemeSearchRequest request, Long userId) {
+        Pageable pageable = PageRequest.of(request.getOffset(), request.getLimit());
+        return themeRepository.searchWithSort(
+                request.getKeyword(),
+                request.getGenre(),
+                userId,
+                request.getSortBy(),
+                pageable
+        );
+    }
+}
