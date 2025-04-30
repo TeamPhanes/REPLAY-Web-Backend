2c209a8ae65e3bfba0ca61bbe4e84c3a5bf276fb
rave189 <rave189@naver.com>
2025-04-30 11:46:06 +0900

    add achievement field

--- src/main/java/phanes/replay/user/dto/UserDTO.java
+++ src/main/java/phanes/replay/user/dto/UserDTO.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
@@ -27,4 +28,5 @@ public class UserDTO {
     private Long failCount;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+    private List<String> representAchievement;
 }
