c648734da6547aa979dec4a639e396d205c23b12
rave189 <rave189@naver.com>
2025-05-03 19:43:38 +0900

    add view

--- src/main/resources/db/migration/h2/V2__create_view.sql
+++ src/main/resources/db/migration/h2/V2__create_view.sql
@@ -1,6 +1,13 @@
+CREATE VIEW theme_with_genres AS
+SELECT *,
+       (SELECT GROUP_CONCAT(g.name SEPARATOR ',')
+        FROM genre g
+        WHERE t.id = g.theme_id) AS genres
+FROM theme t;
+
 CREATE VIEW participate_theme_summary AS
 SELECT p.user_id,
-       t.id                       AS theme_id,
+       t.id                                               AS theme_id,
        t.address,
        t.spot,
        t.cafe,
@@ -8,16 +15,14 @@ SELECT p.user_id,
        t.image,
        t.level,
        t.playtime,
-       (SELECT GROUP_CONCAT(g.name SEPARATOR ',')
-        FROM genre g
-        WHERE t.id = g.theme_id)  AS genres,
+       t.genres,
        CAST((SELECT COALESCE(AVG(r2.score), 0)
-        FROM review r2
-        WHERE r2.theme_id = t.id) AS NUMERIC(10, 2)) AS total_rating,
-       r.id                       AS review_id,
+             FROM review r2
+             WHERE r2.theme_id = t.id) AS NUMERIC(10, 2)) AS total_rating,
+       r.id                                               AS review_id,
        (SELECT COUNT(*)
         FROM review r3
-        WHERE r3.theme_id = t.id) AS review_count,
+        WHERE r3.theme_id = t.id)                         AS review_count,
        r.score,
        r.hint,
        r.number_of_player,
@@ -27,5 +32,23 @@ SELECT p.user_id,
        r.content,
        r.success
 FROM participating_theme p
-         JOIN theme t ON p.theme_id = t.id
+         JOIN theme_with_genres t ON p.theme_id = t.id
          LEFT JOIN review r ON t.id = r.theme_id;
+
+CREATE OR REPLACE VIEW participate_gathering_summary AS
+SELECT gm.user_id,
+       g.id   AS gathering_id,
+       g.name,
+       t.address,
+       t.spot,
+       t.cafe,
+       t.id   AS theme_id,
+       t.name AS theme_name,
+       t.image,
+       t.genres,
+       t.level,
+       g.date_time,
+       g.capacity
+FROM gathering_member gm
+         JOIN gathering g ON gm.gathering_id = g.id
+         JOIN theme_with_genres t ON g.theme_id = t.id;
