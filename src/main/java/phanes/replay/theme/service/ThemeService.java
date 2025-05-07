e720354617097983220dae7a958baba1b587ae87
PeterShin <daehun47@gmail.com>
2025-05-08 02:11:20 +0900

    Revert "refactor themeservice"
    This reverts commit 59dd35af2c2c8cdac524c27fab2c52c502388493.

--- src/main/java/phanes/replay/theme/service/ThemeService.java
+++ src/main/java/phanes/replay/theme/service/ThemeService.java
@@ -2,14 +2,10 @@ package phanes.replay.theme.service;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import phanes.replay.theme.domain.ThemeLikeView;
-import phanes.replay.theme.dto.ThemeListResponse;
-import phanes.replay.theme.dto.ThemeSearchRequest;
 import phanes.replay.theme.repository.ThemeLikeViewRepository;
-import phanes.replay.theme.repository.ThemeRepository;
 
 import java.util.List;
 
@@ -18,20 +14,8 @@ import java.util.List;
 public class ThemeService {
 
     private final ThemeLikeViewRepository themeLikeViewRepository;
-    private final ThemeRepository themeRepository;
 
     public List<ThemeLikeView> getUserLikeTheme(Long userId, Pageable pageable) {
         return themeLikeViewRepository.findByUserId(userId, pageable);
-    }
-
-    public List<ThemeListResponse> getThemes(ThemeSearchRequest request, Long userId) {
-        Pageable pageable = PageRequest.of(request.getOffset(), request.getLimit());
-        return themeRepository.searchWithSort(
-                request.getKeyword(),
-                request.getGenre(),
-                userId,
-                request.getSortBy(),
-                pageable
-        );
     }
 }
