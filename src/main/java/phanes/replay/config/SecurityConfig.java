3b0c3629ad769e04d29d87b0f3ea657afde0128f
PeterShin <daehun47@gmail.com>
2025-05-08 02:18:26 +0900

    refactor securityconfig

--- src/main/java/phanes/replay/config/SecurityConfig.java
+++ src/main/java/phanes/replay/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/v3/api-docs.yaml",
                                 "/gathering/**",
-                                "/room/**"
+                                "/room/**",
                                 "/user/**"
                         ).permitAll()
                         .anyRequest().authenticated()
