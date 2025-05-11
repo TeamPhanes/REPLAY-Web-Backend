f7964691f67b48571b629c3f17038648a063b716
rave189 <59406960+rave189@users.noreply.github.com>
2025-05-11 22:19:59 +0900

    Feature: gathering member (#48)
    * add get gathering member api
    
    * add gathering member api
    
    * add delete gathering member api

---
+++ src/main/java/phanes/replay/gathering/mapper/GatheringMemberMapper.java
@@ -0,0 +1,18 @@
+package phanes.replay.gathering.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import phanes.replay.gathering.domain.Gathering_Member;
+import phanes.replay.gathering.dto.response.GatheringMemberRs;
+
+@Mapper(componentModel = "spring")
+public interface GatheringMemberMapper {
+
+    @Mapping(target = "nickname", source = "user.nickname")
+    @Mapping(target = "image", source = "user.profileImage")
+    @Mapping(target = "comment", source = "user.profileComment")
+    @Mapping(target = "createdAt", source = "user.createdAt")
+    @Mapping(target = "updatedAt", source = "user.updatedAt")
+    @Mapping(target = "representAchievement", expression = "java(java.util.Collections.emptyList())")
+    GatheringMemberRs toGatheringRs(Gathering_Member gatheringMember);
+}
