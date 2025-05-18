27a54f8050b8402653464e1b0ea8281dcc84b762
rave189 <rave189@naver.com>
2025-05-18 21:55:56 +0900

    remove unused class

--- src/main/java/phanes/replay/theme/repository/ThemeWithGenreViewRepository.java
+++
@@ -1,7 +0,0 @@
-package phanes.replay.theme.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import phanes.replay.theme.domain.ThemeWithGenreView;
-
-public interface ThemeWithGenreViewRepository extends JpaRepository<ThemeWithGenreView, Long> {
-}
