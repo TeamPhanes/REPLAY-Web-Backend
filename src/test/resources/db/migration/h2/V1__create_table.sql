58b1ff646a6442fb78d2a27bfa9ed150596c94ec
rave189 <rave189@naver.com>
2025-06-05 23:23:47 +0900

    add individual type value

--- src/test/resources/db/migration/h2/V1__create_table.sql
+++ src/test/resources/db/migration/h2/V1__create_table.sql
@@ -126,11 +126,12 @@ CREATE TABLE gathering_member
 
 CREATE TABLE gathering_content
 (
-    id           IDENTITY PRIMARY KEY,
-    content      VARCHAR(255) DEFAULT '모임 소개글입니다',
-    gathering_id BIGINT,
-    price        VARCHAR(8),
-    image        VARCHAR(200),
+    id            IDENTITY PRIMARY KEY,
+    content       VARCHAR(255) DEFAULT '모임 소개글입니다',
+    gathering_id  BIGINT,
+    price         VARCHAR(8),
+    is_individual BOOLEAN,
+    image         VARCHAR(200),
     FOREIGN KEY (gathering_id) REFERENCES gathering (id)
 );
 
