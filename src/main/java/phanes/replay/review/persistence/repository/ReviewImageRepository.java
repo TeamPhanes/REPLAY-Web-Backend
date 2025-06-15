d2903e5c7ac39e48b61ef75acf77cd64c96acd80
rave189 <rave189@naver.com>
2025-06-15 18:26:59 +0900

    fix review delete

--- src/main/java/phanes/replay/review/persistence/repository/ReviewImageRepository.java
+++ src/main/java/phanes/replay/review/persistence/repository/ReviewImageRepository.java
@@ -3,5 +3,8 @@ package phanes.replay.review.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import phanes.replay.review.domain.ReviewImage;
 
+import java.util.List;
+
 public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
+    List<ReviewImage> findByReviewId(Long reviewId);
 }
