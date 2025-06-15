d7f84ffc1a62ebe98c6fac885de6ef7fee6d0d2c
rave189 <rave189@naver.com>
2025-06-15 13:59:59 +0900

    delete gathering like optimize query

--- src/main/java/phanes/replay/gathering/persistence/repository/GatheringLikeRepository.java
+++ src/main/java/phanes/replay/gathering/persistence/repository/GatheringLikeRepository.java
@@ -1,6 +1,7 @@
 package phanes.replay.gathering.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import phanes.replay.gathering.domain.GatheringLike;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.Optional;
 public interface GatheringLikeRepository extends JpaRepository<GatheringLike, Long> {
 
     Long countByUserId(Long userId);
+    @Query("SELECT gl FROM GatheringLike gl JOIN FETCH gl.user u JOIN FETCH gl.gathering g WHERE u.id = :userId AND g.id = :gatheringId")
     Optional<GatheringLike> findByUserIdAndGatheringId(Long userId, Long gatheringId);
 
     List<GatheringLike> findAllByGatheringId(Long gatheringId);
