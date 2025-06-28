41f2590a66820300087f5962fabf97c7af7815aa
rave189 <rave189@naver.com>
2025-06-29 00:57:35 +0900

    refactor code

--- src/main/java/phanes/replay/gathering/service/GatheringCommentQueryService.java
+++ src/main/java/phanes/replay/gathering/service/GatheringCommentQueryService.java
@@ -3,11 +3,11 @@ package phanes.replay.gathering.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import phanes.replay.exception.CommentNotFoundException;
 import phanes.replay.gathering.domain.GatheringComment;
 import phanes.replay.gathering.persistence.repository.GatheringCommentRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,19 +16,24 @@ public class GatheringCommentQueryService {
     private final GatheringCommentRepository gatheringCommentRepository;
 
     public List<GatheringComment> findByUserId(Long userId, Pageable pageable) {
-        return gatheringCommentRepository.findByUserId(userId, pageable);
+        return gatheringCommentRepository.findByUserId(userId, pageable)
+                .stream()
+                .toList();
     }
 
     public List<GatheringComment> findAllByGatheringId(Long gatheringId) {
         return gatheringCommentRepository.findAllByGatheringId(gatheringId);
     }
 
-    public List<GatheringComment> findByGatheringId(Long gatheringId, Pageable pageable) {
-        return gatheringCommentRepository.findByGatheringId(gatheringId, pageable);
+    public List<GatheringComment> findByGatheringIdWithUserAndGathering(Long gatheringId, Pageable pageable) {
+        return gatheringCommentRepository.findAllByGatheringId(gatheringId, pageable)
+                .stream()
+                .toList();
     }
 
-    public Optional<GatheringComment> findByIdAndGatheringIdAndUserId(Long commentId, Long gatheringId, Long userId) {
-        return gatheringCommentRepository.findByIdAndGatheringIdAndUserId(commentId, gatheringId, userId);
+    public GatheringComment findByIdAndGatheringIdAndUserId(Long commentId, Long gatheringId, Long userId) {
+        return gatheringCommentRepository.findByIdAndGatheringIdAndUserId(commentId, gatheringId, userId)
+                .orElseThrow(() -> new CommentNotFoundException(String.format("user: %d, gathering: %d, comment: %d not found", userId, gatheringId, commentId)));
     }
 
     public Long countByUserId(Long userId) {
