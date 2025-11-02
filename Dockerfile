59d0b46f180ca193571974c5e73a559f8df8af5c
rave189 <rave189@naver.com>
2025-11-02 12:15:14 +0900

    fix build error

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM amazoncorretto:21
-COPY /build/libs/REPLAY-Web-Backend-0.0.1-SNAPSHOT.jar /backend.jar
+COPY /build/libs/replay-0.0.1-SNAPSHOT.jar /backend.jar
 ENTRYPOINT ["java", "-jar", "backend.jar"]
--- src/main/java/phanes/replay/user/persistence/mapper/UserMapper.java
+++ src/main/java/phanes/replay/user/persistence/mapper/UserMapper.java
@@ -1,11 +1,13 @@
 package phanes.replay.user.persistence.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import phanes.replay.user.domain.User;
 import phanes.replay.user.dto.user.UserRs;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(source = "profileImage", target = "image")
     UserRs toUserRs(User user);
 }
