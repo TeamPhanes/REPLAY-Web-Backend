d6299916eb29d047c3dc3eb8be7b942eaa7f46e2
rave189 <rave189@naver.com>
2025-11-16 18:27:59 +0900

    fix subquery context

--- src/main/java/phanes/replay/common/utils/JooqRepositoryUtils.java
+++ src/main/java/phanes/replay/common/utils/JooqRepositoryUtils.java
@@ -1,7 +1,5 @@
 package phanes.replay.common.utils;
 
-import lombok.RequiredArgsConstructor;
-import org.jooq.DSLContext;
 import org.jooq.Field;
 import org.jooq.impl.DSL;
 import org.springframework.stereotype.Component;
@@ -13,13 +11,10 @@ import static phanes.replay.tables.ThemeLike.THEME_LIKE;
 import static phanes.replay.tables.ThemeVisit.THEME_VISIT;
 
 @Component
-@RequiredArgsConstructor
 public class JooqRepositoryUtils {
 
-    private final DSLContext dsl;
-
     public Field<Boolean> isLikedTheme(Long userId) {
-        return DSL.exists(dsl.selectOne()
+        return DSL.exists(DSL.selectOne()
                         .from(THEME_LIKE)
                         .where(THEME_LIKE.THEME_ID.eq(THEME.ID)
                                 .and(THEME_LIKE.USER_ID.eq(userId))))
@@ -27,7 +22,7 @@ public class JooqRepositoryUtils {
     }
 
     public Field<Boolean> isVisitedTheme(Long userId) {
-        return DSL.exists(dsl.selectOne()
+        return DSL.exists(DSL.selectOne()
                         .from(THEME_VISIT)
                         .where(THEME_VISIT.THEME_ID.eq(THEME.ID)
                                 .and(THEME_VISIT.USER_ID.eq(userId))))
@@ -35,7 +30,7 @@ public class JooqRepositoryUtils {
     }
 
     public Field<Boolean> isLikedGathering(Long userId) {
-        return DSL.exists(dsl.selectOne()
+        return DSL.exists(DSL.selectOne()
                         .from(GATHERING_LIKE)
                         .where(GATHERING_LIKE.GATHERING_ID.eq(GATHERING.ID)
                                 .and(GATHERING_LIKE.USER_ID.eq(userId))))
