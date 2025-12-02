f004ab6fadb403f2ccdc209b8bb7ded6bfd7a002
rave189 <rave189@naver.com>
2025-12-02 23:08:37 +0900

    add orderby

--- src/main/java/phanes/replay/review/repository/ReviewImageJooqRepository.java
+++ src/main/java/phanes/replay/review/repository/ReviewImageJooqRepository.java
@@ -19,7 +19,7 @@ public class ReviewImageJooqRepository {
         return dsl.select(REVIEW_IMAGE.REVIEW_ID, REVIEW_IMAGE.IMAGE)
                 .from(REVIEW_IMAGE)
                 .where(REVIEW_IMAGE.REVIEW_ID.in(reviewIdList))
-                .orderBy(REVIEW_IMAGE.IS_REPRESENTATIVE.desc())
+                .orderBy(REVIEW_IMAGE.IS_REPRESENTATIVE.desc(), REVIEW_IMAGE.ID.asc())
                 .fetchGroups(REVIEW_IMAGE.REVIEW_ID, REVIEW_IMAGE.IMAGE);
     }
 }
